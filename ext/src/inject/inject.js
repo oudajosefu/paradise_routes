f2b034a81894cb2572b91694711f55600a022b58
ジョセフ <40701096+oudajosefu@users.noreply.github.com>
2020-06-28 22:35:42 -0700

    Gets lat and long of orders

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -7,7 +7,17 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 
-		console.log($("#order-map-canvas").attr("data-orders"));
+		data_orders_json = JSON.parse($("#order-map-canvas").attr("data-orders"));
+		console.log(data_orders_json);
+
+		for (let i = 0; i < data_orders_json.length; i++) {
+			const element = data_orders_json[i];
+			console.log(`
+latitude: ${element.latitude}
+longitude: ${element.longitude}
+			`);
+			
+		}
 		// ----------------------------------------------------------
 
 	}
