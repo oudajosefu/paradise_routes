b584dfa2f9dd585cc11f87d2a377c22e58af27a1
oudajosefu <odeh.joseph@gmail.com>
2020-07-03 23:59:34 -0700

    Uses jquery's ajax for distance matrix request

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -2,21 +2,15 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'orders') {
     let orders = request.data;
-    console.log(orders);
+    // console.log(orders);
     // let orders = [{address: '14423 N 73rd Ln'}];
     sendResponse({message: 'Received!'});
 
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${encodeURI('STORE ADDRESS')}&destinations=${encodeURI(orders[0].address)}&key=MY_API_KEY`, true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState == 4) {
-        let resp = JSON.parse(xhr.responseText);
-        console.log(resp);
-
-        // Use resp.rows[0].elements[0].duration.text or ''.value for the duration in minutes or seconds
-        // Use orders[i].address for each address.
-      }
-    }
-    xhr.send();
+    $.ajax({
+      url: `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${encodeURI('STORE ADDRESS')}&destinations=${encodeURI(orders[0].address)}&key=MY_API_KEY`,
+      success: (result) => { console.log(result); }
+      // Use result.rows[0].elements[0].duration.text or ''.value for the duration in minutes or seconds
+      // Use orders[i].address for each address.
+    });
   }
 });
