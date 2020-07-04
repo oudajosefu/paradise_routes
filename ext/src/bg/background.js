2489fa51090504dd8450f7d77c450e32da1c752a
ジョセフ <40701096+oudajosefu@users.noreply.github.com>
2020-07-04 13:07:12 -0700

    Adds testing and comments for algorithm

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -1,16 +1,30 @@
 //example of using a message handler from the inject scripts
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'orders') {
+    let apiKey = 'MY_API_KEY';
+    let storeAddress = 'STORE_ADDRESS';
     let orders = request.data;
-    // console.log(orders);
-    // let orders = [{address: '14423 N 73rd Ln'}];
+
+    // Uncomment for testing purposes:
+    // orders = [{name: "John Doe", address: "2130 East Nighthawk Way", age: "6m"}, {name: 'Ashley Dole', address: '2700 North Hayden Road 3083', age: '20m'}];
+    // storeAddress = 'Paradise Liquor Mini Mart';
+    // apiKey = 'xxx';
+    // console.log(JSON.stringify(orders));
+
     sendResponse({message: 'Received!'});
 
     $.ajax({
-      url: `https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${encodeURI('STORE ADDRESS')}&destinations=${encodeURI(orders[0].address)}&key=MY_API_KEY`,
+      url: 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=' +
+            encodeURI(storeAddress) + '&destinations=' +
+            encodeURI(orders[0].address) + '&key=' +
+            apiKey,
       success: (result) => { console.log(result); }
-      // Use result.rows[0].elements[0].duration.text or ''.value for the duration in minutes or seconds
+      // Use result.rows[0].elements[0].duration.text or ''.value for the duration in minutes or seconds respectively.
       // Use orders[i].address for each address.
+
+      // Algorithm:
+      // Request duration for each address from Paradise and append the shortest one to array, request duration for every other address from first address and append shortest one to array.
+      // Continue until total duation of route to all addresses is under 60 minutes. Factor in the age of the order in each calculation to make sure longer aged orders have more priority.
     });
   }
 });
